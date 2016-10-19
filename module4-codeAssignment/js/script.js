d4dff7b3450a97363cd96668de7ce78ea349c7a0
Vanderlei Vinicius <vanderleivoliveiras@gmail.com>
2016-10-19 20:24:41 -0200

    provavel solução 2

---
+++ module4-codeAssignment/js/script.js
@@ -0,0 +1,29 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// STEP 10:
+// Loop over the names array and say either 'Hello' or "Good Bye"
+// using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// 'speak' method.
+// See Lecture 50, part 1
+for (int i = 0; names.length > i; i++) {
+
+  // STEP 11:
+  // Retrieve the first letter of the current name in the loop.
+  // Use the string object's 'charAt' function. Since we are looking for
+  // names that start with either upper case or lower case 'J'/'j', call
+  // string object's 'toLowerCase' method on the result so we can compare
+  // to lower case character 'j' afterwards.
+  // Look up these methods on Mozilla Developer Network web site if needed.
+  // var firstLetter =
+
+  // STEP 12:
+  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+  // name in the loop.
+  if (name[i].slice(0,1) == 'j' || name[i].slice(0,1) == 'J') {
+    // byeSpeaker.xxxx
+  } else {
+    // helloSpeaker.xxxx
+  }
+}
